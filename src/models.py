e47390bdb768115f545e2d08536558f6b9cdaeb2
Andymms <andreamm0714@gmail.com>
2026-01-02 16:17:29 +0000

    Proyecto Finalizado

--- src/models.py
+++ src/models.py
@@ -1,19 +1,44 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+import enum
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    __tablename__ = 'user'
+    id: Mapped[int] = mapped_column(primary_key = True)
+    username: Mapped[str] = mapped_column(String(30), nullable= False) 
+    firstname: Mapped[str] = mapped_column(String(25), nullable= False)
+    lastname: Mapped[str] = mapped_column(String(25), nullable= False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique = True, nullable= False)
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable= False)
 
+class Followers(db.Model):
+    __tablename__ = 'followers'
+    user_from_id: Mapped[int] = mapped_column(ForeignKey('user.id'), primary_key = True)
+    user_to_id: Mapped[int] = mapped_column(ForeignKey('user.id'), primary_key = True)
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Post(db.Model):
+    __tablename__ = 'post'
+    id: Mapped[int] = mapped_column(primary_key = True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+
+class MediaEnum(enum.Enum):
+    PHOTO = 'photo'
+    VIDEO = 'video'
+
+class Media(db.Model):
+    __tablename__ = 'media'
+    id: Mapped[int] = mapped_column(primary_key = True)
+    type: Mapped[MediaEnum] = mapped_column(db.Enum(MediaEnum), nullable= False)
+    url: Mapped[str] = mapped_column(String(120))
+    post_id: Mapped[int] = mapped_column(ForeignKey('post.id'))
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    id: Mapped[int] = mapped_column(primary_key = True)
+    comment_text: Mapped[str] = mapped_column(String(300), nullable= False)
+    author_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    post_id: Mapped[int] = mapped_column(ForeignKey('post.id'))
